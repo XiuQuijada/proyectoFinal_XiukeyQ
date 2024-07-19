c2c4ec0849083c322feecd2094e9d7dbfa8cb1bb
Xiu Quijada <118984620+XiuQuijada@users.noreply.github.com>
2024-07-19 19:56:55 +0200

    creo en js la funcion appear para que los elementos se vayan apareciendo al hacer scroll

--- js/index.js
+++ js/index.js
@@ -20,4 +20,22 @@ pestaña.forEach((titulo, i) => {
     })
 })
 
-/* Parallax */
+/* Appear */
+
+//Declaro variable para la clase "appear"
+let appearElement = document.querySelectorAll('.appear')
+
+//Evento de "escucha" a la ventana 
+window.addEventListener('scroll', () =>{
+    //se ejecuta para cada elemento con la clase appear
+    appearElement.forEach( (appear) =>{
+        //Si el elemento esta visible se resta 150 a la altura de la ventana
+        if (appear.getBoundingClientRect().top < window.innerHeight - 150 ) {
+            appear.style.opacity = '1'  //y aparece el elemento 
+           //Si el elemento no esta visible se remueve el atributo style
+        } else {
+            appear.removeAttribute('style')
+        }
+    } )
+
+})
